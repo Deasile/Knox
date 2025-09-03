fd0e0afdcab06584dfe16a2f958fc9049dd84eb3
Deasile <ravennightspell@gmail.com>
2025-09-02 21:32:54 -0400

    CLEAN UP

--- A_Repo_Knox_Push/js/main.js
+++
@@ -1,2 +0,0 @@
-// Placeholder for site-wide JS functionality
-console.log('D&D Campaign Reference site loaded.');
--- run_local_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-echo Starting local Python HTTP server...
-python run_local_server.py
-pause
